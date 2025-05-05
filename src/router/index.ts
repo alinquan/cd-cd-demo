c27f0dfa3af7c80d74e3b318b6ec8555b83e01cb
林 <15521012174@163.com>
2025-05-05 18:07:54 +0800

    测试路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -23,7 +23,8 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
+//   history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
